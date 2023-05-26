f013c0ced3356d1e13d65c1e0b67e1994ee613d3
abdollahkhan <m.abdollah.khan@gmail.com>
2023-05-26 16:02:15 -0700

    added constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,13 @@
+export const API_CACHE_TAGS = {
+  POKEMON: "pokemon",
+};
+
+export const API_BASE_URL = "https://pokeapi.co/api/v2";
+
+export const HTTP = {
+  POST: "POST",
+  GET: "GET",
+  PUT: "PUT",
+  PATCH: "PATCH",
+  DELETE: "DELETE",
+};
